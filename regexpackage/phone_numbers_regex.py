b73a7bb0b278c98b89633491c2eeeb0fa17f47f2
root <utatsineza>
2024-09-16 17:14:50 +0000

    ok

---
+++ regexpackage/phone_numbers_regex.py
@@ -0,0 +1,13 @@
+mport re
+import json
+
+def PhoneNumbersRegex(data):
+    pattern = r'\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}'  # For phone number extraction
+    matches = re.findall(pattern, data)
+    print(f"Phone numbers: {matches}")
+    
+    # Save the result to JSON
+    with open("phone_numbers_data.json", "w", encoding="utf-8") as f:
+        json.dump(matches, f)
+    print("Phone numbers saved successfully.")
+
